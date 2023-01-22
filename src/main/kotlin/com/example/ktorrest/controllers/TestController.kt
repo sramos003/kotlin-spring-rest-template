c49d9a6c042778a787fef35625cd8fe837520549
SR92695 <steve.ramos@thehartford.com>
2023-01-22 17:19:13 -0500

    adding in first api

--- src/main/kotlin/com/example/ktorrest/controllers/TestController.kt
+++ src/main/kotlin/com/example/ktorrest/controllers/TestController.kt
@@ -1,5 +1,22 @@
 package com.example.ktorrest.controllers
 
+import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
+@RestController
+@RequestMapping(value = ["/test-controller"])
 class TestController {
+    
+    // empty constructor
+    fun TestController() {
+    }
+
+    @GetMapping(value = ["/one"], produces = [MediaType.APPLICATION_JSON_VALUE])
+    fun testRouteOne(): ResponseEntity<String> {
+        return ResponseEntity.ok("My first kotlin API")
+    }
+
 }
