37f5a816e7637ecaf8af1400233606ebc03c9875
SR92695 <steve.ramos@thehartford.com>
2023-01-22 17:02:46 -0500

    initialize repository

---
+++ src/main/kotlin/com/example/ktorrest/controllers/TestController.kt
@@ -0,0 +1,5 @@
+package com.example.ktorrest.controllers
+
+
+class TestController {
+}
---
+++ src/test/kotlin/com/example/ktorrest/KtorRestTemplateApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.ktorrest
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KtorRestTemplateApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
